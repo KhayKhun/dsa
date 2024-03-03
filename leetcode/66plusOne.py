b644e6b8a8f566c2a39a49f502198cf1b476e474
KhayKhun <saikhaykhunmong@gmail.com>
2024-03-03 23:44:31 +0630

    Update 66plusOne.py

--- leetcode/66plusOne.py
+++ leetcode/66plusOne.py
@@ -10,16 +10,31 @@ def plusOne(digits):
     return added_list
 
 # my solution 2 (Beats 93.66% of users with Python)
+
+
 def plusOne(digits):
     carry = 1
     index = len(digits) - 1
     while index > -1:
-        sum = digits[index] + carry 
+        sum = digits[index] + carry
         digits[index] = sum % 10
         carry = sum // 10
         index -= 1
-    if carry: digits.insert(0,carry)
+    if carry:
+        digits.insert(0, carry)
     return digits
 
-digits = [9,9]
+# other solution
+
+
+def plusOne(digits):
+    for i in range(len(digits)-1, -1, -1):
+        if digits[i] == 9:
+            digits[i] = 0
+        else:
+            digits[i] = digits[i] + 1
+            return digits
+    return [1] + digits
+
+digits = [9, 9]
 print(plusOne(digits))
