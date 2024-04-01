3b7a2b64f0fa9efff3dd0b8ea46c157edd7d8753
Sai Khay Khun Mong <saikhaykhunmong@gmail.com>
2024-04-01 23:45:24 +0630

    Create 841. Keys and Rooms.py

---
+++ leetcode/841. Keys and Rooms.py
@@ -0,0 +1,21 @@
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        graph = self.build(rooms)
+        visited = set()
+        stack = [0]
+        while len(stack) > 0:
+            current = stack.pop()
+            visited.add(current)
+
+            for i in graph[current]:
+                if not i in visited:
+                    stack.append(i)
+
+        return len(rooms) == len(visited)
+    
+    def build(self,edges):
+        d = defaultdict(int)
+        for i in range(len(edges)):
+            d[i] = edges[i]
+        return d
+        
