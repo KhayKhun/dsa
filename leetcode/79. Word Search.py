b80ab5f98c9dbd55d5ceb8736c5232b2f5b21fd9
Sai Khay Khun Mong <saikhaykhunmong@gmail.com>
2024-04-04 02:22:47 +0630

    Update 79. Word Search.py

--- leetcode/79. Word Search.py
+++ leetcode/79. Word Search.py
@@ -3,13 +3,19 @@ from typing import List
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         def dfs(r, c, target):
+            # check if position is out of range. or word does not match target
             if not (0 <= r < len(board)) or not (0 <= c < len(board[0])) or board[r][c] != word[target]:
                 return False
+            # board[r][c] matched and, is the last position of the word
             if target == len(word) - 1:
                 return True
+            
+            # temporaly assign board[r][c] as '/'. Store the default value in temp.
             temp, board[r][c] = board[r][c], '/'
             found = dfs(r - 1, c, target + 1) or dfs(r + 1, c, target + 1) or dfs(r, c - 1, target + 1) or dfs(r, c + 1, target + 1)
+            # not found? reassign to default value after the operations
             board[r][c] = temp
+
             return found
 
         for row in range(len(board)):
