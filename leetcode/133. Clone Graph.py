a187b60ed6a0e7d5bd123d4e4c1dd7fc6b00d227
Sai Khay Khun Mong <saikhaykhunmong@gmail.com>
2024-04-13 04:32:03 +0630

    Create 133. Clone Graph.py
    3 fking hours on this problem

---
+++ leetcode/133. Clone Graph.py
@@ -0,0 +1,64 @@
+from typing import Optional
+
+class Node:
+    def __init__(self, val=0, neighbors=None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+
+    def __repr__(self):
+        return '%s' % self.val
+
+    def print_graph(self):
+        def dfs(node, visited):
+            if node is None:
+                return
+
+            print(node, [neighbor.val for neighbor in node.neighbors])
+            visited.add(node.val)
+
+            for neighbor in node.neighbors:
+                if neighbor.val not in visited:
+                    dfs(neighbor, visited)
+
+        visited = set()
+        dfs(self, visited)
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node: return
+        visited = dict()
+
+        def dfs(node):
+            if node.val in visited: return visited[node.val]
+
+            clone = Node(node.val)
+            visited[node.val] = clone
+
+            for neighbor in node.neighbors:
+                clone.neighbors.append(dfs(neighbor))
+
+            return clone
+
+        return dfs(node)
+
+# Example usage:
+
+node1 = Node(1)
+node2 = Node(2)
+node3 = Node(3)
+node4 = Node(4)
+
+# Define neighbors for each node
+node1.neighbors = [node2, node4]
+node2.neighbors = [node1, node3]
+node3.neighbors = [node2, node4]
+node4.neighbors = [node1, node3]
+
+solution = Solution()
+result = solution.cloneGraph(node1)
+
+print("Original Graph:")
+node1.print_graph()
+print("---")
+print("Cloned Graph:")
+result.print_graph()
