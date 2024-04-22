a58a826c1fb3054ada9d8df8c83ab1f2064e5bf5
Sai Khay Khun Mong <saikhaykhunmong@gmail.com>
2024-04-22 23:50:11 +0630

    Update allConstruct.js

--- dp/js/allConstruct.js
+++ dp/js/allConstruct.js
@@ -1,21 +1,16 @@
-// O(n m^2) time, O(m^2) space
-let memo = {}
-let res = []
-function allConstruct(current,target,words){
-    if(target in memo) return memo[target]
-    if(target === ''){
-        res.push(current)
-        return
-    }
-
+function allConstruct(target,words){
+    if(target === '') return [[]]
+    const res = []
     for(let w of words){
         if(target.indexOf(w) === 0){
             const remain = target.slice(w.length)
-            memo[remain] = allConstruct([...current, w], remain, words)
+            const ways = allConstruct(remain,words)
+            const results = ways.map(way => [w, ...way])
+            res.push(...results) // push results's items
         }
     }
-    return
+    return res
 }
 
-console.log(allConstruct([],'eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef',['e','ee','eee','e','eeeeeee','eeeee','eeeeeeeeeeeeeeeeeeeee']))
-console.log(res)
+console.log(allConstruct('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',['e','ee','eee','e','eeeeeee','eeeee','eeeeeeeeeeeeeeeeeeeee']))
+console.log(allConstruct('purple',['purp','p','ur','le','purpl']))
