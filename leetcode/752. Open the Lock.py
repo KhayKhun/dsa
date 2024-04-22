90fa888bea2aa33bbf97400b74471ae4dafc843c
Sai Khay Khun Mong <saikhaykhunmong@gmail.com>
2024-04-22 20:53:16 +0630

    Update 752. Open the Lock.py

--- leetcode/752. Open the Lock.py
+++ leetcode/752. Open the Lock.py
@@ -1,5 +1,6 @@
 from collections import deque
 from typing import List
+
 class Solution:
     def openLock(self, deadends: List[str], target: str) -> int:
         if '0000' in deadends: return -1
@@ -35,3 +36,27 @@ target = "8888"
 print(Solution().openLock(deadends,target))
 print(Solution().openLock(["0201","0101","0102","1212","2002"],"0202"))
 
+# similar, faster
+class Solution:
+    def openLock(self, deadends: List[str], target: str) -> int:
+        if target == '0000':
+            return 0
+        seen = set(deadends)
+        if '0000' in seen:
+            return -1
+        q = deque()
+        q.append(('0000', 0))
+        seen.add('0000')
+        def neis(node):
+            for i in range(4):
+                for d in (-1, 1):
+                    yield node[:i] + str( (int(node[i])+d) % 10) + node[i+1:]
+        while q:
+            node, dist = q.popleft()
+            for nei in neis(node):
+                if nei == target:
+                    return dist + 1
+                if nei not in seen:
+                    seen.add(nei)
+                    q.append((nei, dist + 1))
+        return -1
