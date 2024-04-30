3303cabd8f5ddff4dd17e73abe161b062ff5b40c
Sai Khay Khun Mong <saikhaykhunmong@gmail.com>
2024-04-30 22:02:25 +0630

    -

---
+++ codeforces/Boy or Girl.py
@@ -0,0 +1,4 @@
+s = set(input())
+ans = 'CHAT WITH HER!' if len(s)%2 == 0 else 'IGNORE HIM!'
+
+print(ans)
--- leetcode/746. Min Cost Climbing Stairs.py
+++ leetcode/746. Min Cost Climbing Stairs.py
@@ -2,20 +2,14 @@ from typing import List
 
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        n = len(cost)
-        memo = [None] * n
+        memo = {}
         
         def explore(i):
-            if i >= n:
-                return 0
-            if i in memo:
-                return memo[i]
+            if i in memo: return memo[i]
+            if i >= len(cost): return 0
             
             memo[i] = cost[i] + min(explore(i + 1), explore(i + 2))
             return memo[i]
         
         return min(explore(0), explore(1))
 
-
-cost = [1,100,1,1,1,100,1,1,100,1]
-print(Solution().minCostClimbingStairs(cost))
